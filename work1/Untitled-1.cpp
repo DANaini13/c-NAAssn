e0439902b8576b44eb848b84b7b973181fb03085
NAJiawei <NASoftware_Jiawei@163.com>
2018-03-26 14:48:37 -0700

    work-1

---
+++ work1/Untitled-1.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<time.h>
+#include<stdlib.h>
+using namespace std;
+
+void oput(int a,int b){
+    int c = (a+b)/2;
+    cout<<"The first die comes up:"<<a<<endl<<"The second die comes up:"<<b<<endl<<"Your total roll is:"<<c<<endl;
+}
+
+    void rec(int times, int i,int a[]){
+        if(i>=times){
+           oput(a[0],a[1]);
+            return;
+        }
+        a[i]=(rand() %6)+1;
+        ++i;
+        rec(times,i,a);
+    }
+
+int main(){
+    int times = 2;
+    int a[times];
+    srand((unsigned)time(NULL)); 
+    for(int i = 0; i < 2; ++i){
+        a[i]=(rand() %6)+1;
+    }
+    oput(a[0],a[1]);
+
+//=====================================
+    int i = 0;
+    rec(times,i,a);
+}
+
